8c311604c5a6aa5f97429c81a6a05ee6d0787e41
alejandroagca <alejandroagca1995@gmail.com>
2017-11-20 13:46:42 +0100

    Descarga de imagenes

--- app/src/main/java/com/example/myapplication/DescargaActivity.java
+++ app/src/main/java/com/example/myapplication/DescargaActivity.java
@@ -2,12 +2,57 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class DescargaActivity extends AppCompatActivity {
+import com.jakewharton.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_descarga);
+import okhttp3.OkHttpClient;
+
+public class DescargaActivity extends AppCompatActivity implements View.OnClickListener {
+
+        EditText texto;
+        Button botonImagen;
+        ImageView imagen;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_descarga);
+            texto = (EditText) findViewById(R.id.edtURL);
+            botonImagen = (Button) findViewById(R.id.btnDescargarImagen);
+            botonImagen.setOnClickListener(this);
+            imagen = (ImageView) findViewById(R.id.imgImagen);
+        }
+        @Override
+        public void onClick(View v) {
+            String url = texto.getText().toString();
+            if (v == botonImagen)
+                descargaImagen(url);
+        }
+        private void descargaImagen(String url) {
+
+            /*Picasso.with(this).load(url)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .into(imagen);*/
+
+            //utilizar OkHttp3
+            OkHttpClient client = new OkHttpClient();
+
+            Picasso picasso = new Picasso.Builder(this)
+                    .downloader(new OkHttp3Downloader(client))
+                    .build();
+
+            picasso.with(this).load(url)
+                    .placeholder(R.drawable.placeholder)
+                    .error(R.drawable.error)
+                    .into(imagen);
+
+
+
+
+        }
     }
-}
