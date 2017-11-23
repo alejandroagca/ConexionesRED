126d7db8eb7af17eff2628676c46978885f3a9b7
alejandroagca <alejandroagca1995@gmail.com>
2017-11-23 14:44:55 +0100

    PAL MARTES!

--- app/src/main/java/com/example/myapplication/DescargaActivity.java
+++ app/src/main/java/com/example/myapplication/DescargaActivity.java
@@ -1,15 +1,29 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.jakewharton.picasso.OkHttp3Downloader;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import cz.msebera.android.httpclient.Header;
 import okhttp3.OkHttpClient;
 
 public class DescargaActivity extends AppCompatActivity implements View.OnClickListener {
@@ -17,6 +31,7 @@ public class DescargaActivity extends AppCompatActivity implements View.OnClickL
         EditText texto;
         Button botonImagen;
         ImageView imagen;
+        Button botonFile;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -24,6 +39,8 @@ public class DescargaActivity extends AppCompatActivity implements View.OnClickL
             texto = (EditText) findViewById(R.id.edtURL);
             botonImagen = (Button) findViewById(R.id.btnDescargarImagen);
             botonImagen.setOnClickListener(this);
+            botonFile = findViewById(R.id.btnDescargarFichero);
+            botonFile.setOnClickListener(this);
             imagen = (ImageView) findViewById(R.id.imgImagen);
         }
         @Override
@@ -31,6 +48,8 @@ public class DescargaActivity extends AppCompatActivity implements View.OnClickL
             String url = texto.getText().toString();
             if (v == botonImagen)
                 descargaImagen(url);
+            if(v==botonFile)
+                descargaFichero(url);
         }
         private void descargaImagen(String url) {
 
@@ -55,4 +74,58 @@ public class DescargaActivity extends AppCompatActivity implements View.OnClickL
 
 
         }
+    private void descargaFichero(String url)
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new FileAsyncHttpResponseHandler(/* Context */ this) {
+            ProgressDialog pd;
+            @Override
+            public void onStart() {
+                pd = new ProgressDialog(DescargaActivity.this);
+                pd.setTitle("Please Wait..");
+                pd.setMessage("AsyncHttpResponseHadler is in progress");
+                pd.setIndeterminate(false);
+                pd.setCancelable(false);
+                pd.show();
+                Toast.makeText(DescargaActivity.this, "Descargando...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                Toast.makeText(DescargaActivity.this, "Se ha producido un error", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, File file) {
+                Toast.makeText(DescargaActivity.this, "It just works", Toast.LENGTH_SHORT).show();
+                String destino = "/storage/sdcard/copia_"+file.getName()+".txt";
+                FileInputStream fis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+                    FileWriter fstream = new FileWriter(destino, true);
+                    BufferedWriter out = new BufferedWriter(fstream);
+
+                    String aLine = null;
+                    while ((aLine = in.readLine()) != null) {
+                        out.write(aLine);
+                        out.newLine();
+                    }
+                    in.close();
+                    out.close();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                pd.dismiss();
+            }
+
+        });
+    }
     }
--- app/src/main/java/com/example/myapplication/SubirActivity.java
+++ app/src/main/java/com/example/myapplication/SubirActivity.java
@@ -1,13 +1,88 @@
 package com.example.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class SubirActivity extends AppCompatActivity {
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SubirActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText texto;
+    Button boton;
+    TextView informacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subir);
+        texto = findViewById(R.id.editText);
+        informacion = findViewById(R.id.textView);
+        boton = findViewById(R.id.miButton);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == boton){
+            subida();
+        }
+    }
+    public final static String WEB = "http://192.168.2.40/acceso/upload.php";
+    private void subida() {
+        String fichero = texto.getText().toString();
+        final ProgressDialog progreso = new ProgressDialog(SubirActivity.this);
+        File myFile;
+        Boolean existe = true;
+        myFile = new File(Environment.getExternalStorageDirectory(), fichero);
+        //File myFile = new File("/path/to/file.png");
+        RequestParams params = new RequestParams();
+        try {
+            params.put("fileToUpload", myFile);
+        } catch (FileNotFoundException e) {
+            existe = false;
+            informacion.setText("Error en el fichero: " + e.getMessage());
+            //Toast.makeText(this, "Error en el fichero: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        if (existe)
+            RestClient.post(WEB, params, new TextHttpResponseHandler() {
+                @Override
+                public void onStart() {
+                    // called before request is started
+                    progreso.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progreso.setMessage("Conectando . . .");
+                    //progreso.setCancelable(false);
+                    progreso.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        public void onCancel(DialogInterface dialog) {
+                            RestClient.cancelRequests(getApplicationContext(), true);
+                        }
+                    });
+                    progreso.show();
+                }
+
+                public void onSuccess(int statusCode, Header[] headers, String response) {
+                    // called when response HTTP status is "200 OK"
+                    progreso.dismiss();
+                    informacion.setText(response);
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, String response, Throwable t) {
+                    // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                    progreso.dismiss();
+                    informacion.setText("ERROR: " + response);
+                }
+            });
     }
 }
